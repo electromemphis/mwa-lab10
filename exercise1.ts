4a886f0ea21ec78835c852371f94bc115f755c99
bj <bienjames.octura@gmail.com>
2017-04-18 00:02:48 -0500

    don't use class

--- exercise1.ts
+++ exercise1.ts
@@ -1,25 +1,12 @@
-class BankAccount{
-    money: number = 2000;
-
-    deposit(value:number){
-        this.money = value;
+let bankAccount:{money:number, deposit:(value: number)=>void} ={
+    money: 2000,
+    deposit(value){
+        this.money+=value;
     }
-}
+};
 
-class User{
-    name: string;
-    bankAccount: BankAccount;
-    hobbies: string[];
-
-    constructor(name:string,hobbies:string[]){
-        this.bankAccount=new BankAccount();
-        this.name=name;
-        this.hobbies=hobbies;
-    }
-     
-}
-
-let mySelf = new User("James",["basketball"]);
-mySelf.bankAccount.deposit(100000);
-
-console.log(mySelf);
+let myself:{name:string,bankAccount, hobbies:[string]}={
+    name:"Asaad",
+    bankAccount:bankAccount,
+    hobbies:["violin", "Cooking"]
+};
